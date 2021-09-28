89bf89fbeacf8db32e5fbb285ab84fffdff6c2ea
Lucas <lucascoliveira@outlook.com>
2021-09-27 23:13:22 -0300

    Correção if de seleção

--- GeradorSenha.py
+++ GeradorSenha.py
@@ -23,24 +23,21 @@ def escolha_tipo_letra_e_numero():
 
 
 def somente_numeros():
+    quantidade_caracteres = int(input('Número de caracteres da senha: '))
     for quantidade in range(quantidade_caracteres):
         lista1.append(numero_aleatorio())
     return lista1
 
 
 def somente_letras():
+    quantidade_caracteres = int(input('Número de caracteres da senha: '))
     for quantidade in range(quantidade_caracteres):
         lista1.append(letra_aleatoria())
     return lista1
 
 
-def somente_simbolos():
-    for quantidade in range(quantidade_caracteres):
-        lista1.append(simbolo_aleatorio())
-    return lista1
-
-
 def somente_letra_e_numeros():
+    quantidade_caracteres = int(input('Número de caracteres da senha: '))
     for quantidade in range(quantidade_caracteres):
         seletor = escolha_tipo_letra_e_numero()
         if seletor == '0':
@@ -51,6 +48,7 @@ def somente_letra_e_numeros():
 
 
 def todos_os_caracteres():
+    quantidade_caracteres = int(input('Número de caracteres da senha: '))
     for quantidade in range(quantidade_caracteres):
         seletor = escolha_tipo()
         if seletor == '0':
@@ -64,18 +62,23 @@ def todos_os_caracteres():
 
 print('Selecione a forma que a senha deve ser gerada.')
 
-selecao = input('(l) = letras / (n) = números / (ln) = letras e números / (lns) = letras, números e simbolos: ')
-
+selecao = input('(l) = letras / (n) = números / (ln) = letras e números / (lns) = letras, números e simbolos: ').lower()
 
 lista1 = []
-quantidade_caracteres = int(input('Número de caracteres da senha: '))
 
 if selecao == 'l':
-    senha_lista = somente_numeros()
+    senha_lista = somente_letras()
 elif selecao == 'n':
     senha_lista = somente_numeros()
 elif selecao == 'lns':
     senha_lista = todos_os_caracteres()
+else:
+    print('Valor invalido')
+    exit()
+
+
+
+
 
 
 senhagerada = ''.join(senha_lista)
